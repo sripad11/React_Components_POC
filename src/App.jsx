a7c8f37605ef1d6207af40ce82fc1156d42ba3cc
sripad11 <jevalasripad123@gmail.com>
2025-02-09 21:18:18 +0530

    Lifted state up for both components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import UserInput from "./components/UserInput"
+import Result from "./components/Result";
+
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      const tempUserInput = {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      }
+      return tempUserInput;
+    });
+  }
+
   return (
-    <h1> Hi </h1>
+    <>
+      <UserInput userInput={userInput} handleChange={handleChange} />
+      <Result input={userInput}/>
+    </>
   )
 }
 
